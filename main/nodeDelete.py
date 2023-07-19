7de143645c9e07d61918c24324250395d14d61fa
masa0702 <gp20a163@oecu.jp>
2023-07-20 03:11:36 +0900

    add compileabe_code

--- main/nodeDelete.py
+++ main/nodeDelete.py
@@ -1,4 +1,5 @@
 import logging
+import ast
 from os import path
 from copy import deepcopy
 from astars import AParser, AstAnalyser, AstOperator, ACodeGenerator
@@ -8,10 +9,20 @@ logging.basicConfig(format='%(asctime)s -\n %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', 
                     level=logging.INFO)
 
+def compileabe_code(code):
+    try:
+        ast.parse(code)
+        print("True")
+        return True
+    except SyntaxError:
+        print("False")
+        return False
+    
 def main():
     with open(path.join(path.dirname(__file__), "input", "test.py")) as f:
         code = f.read()
-        
+    
+    compileabe_code(code)
     parser = AParser()
     tree = parser.parse(text=code, lang="python")
     logging.info(RenderTree(tree).by_attr("type"))
